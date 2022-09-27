078f08e4a8611c6de1e287f6b913c2e1d032b943
Afreez <fae@mangdang.net>
2022-09-27 19:12:39 +0800

    Add a work around method to increase the probability of pair the Controller to mini pupper

--- install.sh
+++ install.sh
@@ -28,3 +28,8 @@ sudo ln -s $(realpath .)/robot.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable robot
 sudo systemctl start robot
+
+
+sudo mv restart_joy.service /lib/systemd/system/
+sudo mv joystart.sh /sbin/
+systemctl enable restart_joy
